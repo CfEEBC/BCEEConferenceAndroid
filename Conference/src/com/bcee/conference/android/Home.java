35322e105e05fb4ab7e475c37c392737947e2220
whatisthis123 <daniel9618@gmail.com>
2013-10-25 21:54:23 -0700

    test

--- Conference/src/com/bcee/conference/android/Home.java
+++ Conference/src/com/bcee/conference/android/Home.java
@@ -10,6 +10,8 @@ public class Home extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
+		
+		// making new comment
 	}
 
 	@Override
