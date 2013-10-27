e12fda6335ae4448c2d28d3f8bb127f2bce4a28b
willyao94 <williamyao@live.ca>
2013-10-27 11:31:27 -0700

    Display

--- Conference/gen/com/bcee/conference/android/R.java
+++ Conference/gen/com/bcee/conference/android/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int exp_list_back=0x7f070000;
+        public static final int exp_list_back=0x7f040001;
+        public static final int text_colour_orange=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -20,8 +21,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int bccfee_logo=0x7f020000;
@@ -65,11 +66,11 @@ public final class R {
         public static final int sessions=0x7f080004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int textview_string=0x7f050004;
-        public static final int title_activity_conferences=0x7f050003;
-        public static final int title_activity_expandable_list=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int textview_string=0x7f060004;
+        public static final int title_activity_conferences=0x7f060003;
+        public static final int title_activity_expandable_list=0x7f060002;
     }
     public static final class style {
         /** 
@@ -91,12 +92,12 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
