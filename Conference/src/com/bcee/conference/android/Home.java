a67dddba4124855c537b628f225a320904eef265
whatisthis123 <daniel9618@gmail.com>
2013-10-26 19:45:23 -0700

    -fixed android.os.networkexception

--- Conference/src/com/bcee/conference/android/Home.java
+++ Conference/src/com/bcee/conference/android/Home.java
@@ -5,6 +5,7 @@ import com.bcee.conference.android.models.DataCentre;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
@@ -17,15 +18,18 @@ public class Home extends Activity {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.home);
-		DataCentre dc = DataCentre.createDefaultInstance();
+		DataCentre.createDefaultInstance();
 		initvars();
+
+
+
 	}
 
 	private void initvars() {
 		// TODO Auto-generated method stub
 		Button sessions = (Button) findViewById(R.id.welcomeB1);
 		sessions.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
--- Conference/src/com/bcee/conference/android/models/DataCentre.java
+++ Conference/src/com/bcee/conference/android/models/DataCentre.java
@@ -30,7 +30,7 @@ import android.util.Log;
 public class DataCentre {
 
 	private static final String JSON_URL = "http://bceeconference.appspot.com/machine";
-	
+
 	/**
 	 * the DataCentre
 	 */
@@ -52,13 +52,13 @@ public class DataCentre {
 		}
 		return defaultInstance;
 	}
-	
+
 	/**
 	 * get conferences that starts at a particular time 
 	 * @param time conference's start time
 	 * @return a list of names of conferences starting at a particular time 
 	 */
-	
+
 	public static List<String> getDataByTime(String time) {
 		// TODO Auto-generated method stub
 		List<String> arr = new ArrayList<String>();
@@ -75,41 +75,48 @@ public class DataCentre {
 	 * Starts parsing our data that we get from the server to populate fields in our ConferenceModel
 	 */
 	public static void parseData(){
-		HttpClient client = new DefaultHttpClient();  
+		final HttpClient client = new DefaultHttpClient();  
 		String url = JSON_URL;   
-		HttpGet httpget = new HttpGet(url);
-		ResponseHandler<String> handler = new BasicResponseHandler();
-		try{
-			String jsonString = client.execute(httpget,handler);
-			JSONArray arr = new JSONArray(jsonString);
-			//  gets the fields 
-			for(int i=0;i<arr.length();i++){
-				JSONObject obj = (JSONObject) arr.get(i);
-				String name = (String) obj.get("session_name");
-				String location = (String) obj.get("location");
-				// truncate strings into "MM-DD hh:mm"
-				String start = obj.getString("stime").substring(5,obj.getString("stime").length()-3);
-				String end = obj.getString("etime").substring(5,obj.getString("etime").length()-3);
-				String descrip = obj.getString("description");
-				String speakers = obj.getString("speakers");
-				String bio = obj.getString("biography");
-				String survey = httpCheck(obj.getString("survey_link"));
-				ConferenceModel c = new ConferenceModel(name,descrip,location,speakers,bio,start,end,survey);
-				System.out.println(models==null);
-				models.add(c);  // adds the new ConferenceModel to our collection
-				Log.d("tag",start);
+		final HttpGet httpget = new HttpGet(url);
+		final ResponseHandler<String> handler = new BasicResponseHandler();
+		//try{
+		//final String jsonString ="";
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					String jsonString = client.execute(httpget,handler);
+					JSONArray arr = new JSONArray(jsonString);
+					//  gets the fields 
+					for(int i=0;i<arr.length();i++){
+						JSONObject obj = (JSONObject) arr.get(i);
+						String name = (String) obj.get("session_name");
+						String location = (String) obj.get("location");
+						// truncate strings into "MM-DD hh:mm"
+						String start = obj.getString("stime").substring(5,obj.getString("stime").length()-3);
+						String end = obj.getString("etime").substring(5,obj.getString("etime").length()-3);
+						String descrip = obj.getString("description");
+						String speakers = obj.getString("speakers");
+						String bio = obj.getString("biography");
+						String survey = httpCheck(obj.getString("survey_link"));
+						ConferenceModel c = new ConferenceModel(name,descrip,location,speakers,bio,start,end,survey);
+						System.out.println(models==null);
+						models.add(c); 
+					}
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+
 			}
-		} catch(JSONException e){
-			Log.d("LOG_TAG",e.getMessage());
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		}).start();
 	}
-	
+
 	/**
 	 * Checks if the string url has http header
 	 * @param s the url string
@@ -120,7 +127,7 @@ public class DataCentre {
 			return s;
 		else return ("http://" + s);
 	}
-	
+
 	/**
 	 * Find the ConferenceModel by startTime and name 
 	 * @param start Conference startTime
@@ -135,7 +142,7 @@ public class DataCentre {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Get all the startTime as a collection of strings
 	 * @return all startTime as collection of strings
@@ -147,4 +154,7 @@ public class DataCentre {
 		}
 		return s;
 	}
+
+
+
 }
